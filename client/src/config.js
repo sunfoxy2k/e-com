5a339dd8761fec9e88682eea08c999bab7782a35
sunfoxy2k <sunfoxy2k@gmail.com>
2022-11-29 01:02:23 +0700

    server endpoint

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-export const backendAddress = 'http://localhost:3030';
+export const backendAddress = 'http://54.151.190.141:3030';
 // export const backendAddress = IS_PRODUCTION ? "https://e-sushi-be.herokuapp.com" : 'http://localhost:3030';
